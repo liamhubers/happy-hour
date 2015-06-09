a22e4225d04a86f11d1f67ce5402e3cd3565c9fd
Liam Hubers <liam.hubers@gmail.com>
2015-06-09 22:17:14 +0200

    added current position to map and disabled back after login

--- app/src/main/java/com/school/guidoschmitz/happyhours/activities/LoginActivity.java
+++ app/src/main/java/com/school/guidoschmitz/happyhours/activities/LoginActivity.java
@@ -1,8 +1,16 @@
 package com.school.guidoschmitz.happyhours.activities;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 
 import com.facebook.CallbackManager;
@@ -13,7 +21,10 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.school.guidoschmitz.happyhours.R;
 
-public class LoginActivity extends FragmentActivity {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class LoginActivity extends FragmentActivity implements LocationListener {
 
     private static final String TAG = "FacebookLogin";
 
@@ -43,6 +54,23 @@ public class LoginActivity extends FragmentActivity {
                 Log.i(TAG, "Something went wrong");
             }
         });
+
+        getHash();
+    }
+
+    private void getHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.school.guidoschmitz.happyhours", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
 
     @Override
@@ -51,6 +79,27 @@ public class LoginActivity extends FragmentActivity {
         this.callbackManager.onActivityResult(requestCode, resultCode, data);
 
         Intent intent = new Intent(getBaseContext(), MapActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
--- app/src/main/java/com/school/guidoschmitz/happyhours/activities/MapActivity.java
+++ app/src/main/java/com/school/guidoschmitz/happyhours/activities/MapActivity.java
@@ -1,9 +1,14 @@
 package com.school.guidoschmitz.happyhours.activities;
 
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,6 +16,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.school.guidoschmitz.happyhours.R;
@@ -19,6 +25,7 @@ import com.school.guidoschmitz.happyhours.R;
 public class MapActivity extends ActionBarActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap map;
+    private final int INITIAL_ZOOM = 16;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +66,7 @@ public class MapActivity extends ActionBarActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.setMyLocationEnabled(true);
 
         LatLng latLng1 = new LatLng(52.080182, 4.316461);
         LatLng latLng2 = new LatLng(52.081038, 4.315759);
@@ -73,7 +81,30 @@ public class MapActivity extends ActionBarActivity implements OnMapReadyCallback
 
         map.setOnMarkerClickListener(this);
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng1, 15));
+        /* zoom to current location */
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        String provider = locationManager.getBestProvider(new Criteria(), true);
+        Location location = locationManager.getLastKnownLocation(provider);
+        if(location != null){
+            map.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(location.getLatitude(), location.getLongitude()),
+                    INITIAL_ZOOM
+                )
+            );
+        }
+        GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+            private boolean setLocation = false;
+            @Override
+            public void onMyLocationChange (Location location) {
+                if(!setLocation) {
+                    LatLng locaction = new LatLng(location.getLatitude(), location.getLongitude());
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(locaction, INITIAL_ZOOM));
+                    setLocation = true;
+                }
+            }
+        };
+        map.setOnMyLocationChangeListener(myLocationChangeListener);
     }
 
     @Override
